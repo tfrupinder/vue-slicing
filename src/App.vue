a76acd8ad7b29fa7be308b9d3ae44dbb6dc1a1d2
tfrupinder <techformation.rupinder@gmail.com>
2020-06-16 17:47:48 +0530

    Home page (in-progress)

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a href="#" class="btn navbar-brand">Logo Here</a>
+      <div class="navbar-nav ml-auto">
+        <li class="nav-item ml-auto mx-auto w-100px">
+          <div class="dropdown">
+            <a class="btn btn-none dropdown-toggle" data-toggle="collapse" data-target="#collapsibleNavbar">
+              Eng
+            </a>
+            <div class="dropdown-menu" id="collapsibleNavbar">
+              <a class="dropdown-item" href="#">Link 1</a>
+              <a class="dropdown-item" href="#">Link 2</a>
+              <a class="dropdown-item" href="#">Link 3</a>
+            </div>
+          </div>
+        </li>
+        <li class="nav-item mx-auto w-100px">
+          <span class="navbar-toggler-icon"></span>
+        </li>
+      </div>
+    </nav>
+
+    <div>
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,194 @@
+<template>
+  <div>
+    <div class="bg-blue">
+      <div class="container p-0">
+      <div class="row p-0 m-0">
+        <p class="text-white ml-0 mb-0 mt-5">Services</p>
+        </div>
+        <p class="text-white sl ml-0">
+          Start creating or <br />
+          optimizing existing <br />
+          systems with our <br />
+          services
+        </p>
+        <div class="card-deck services-cards m-0">
+          <div class="w-25 bg-white">
+            <div class="p-3">
+              <img src="../assets/fileLogo.png" class="img-size" />
+              <p class="services-content mt-4">
+                Web <br />
+                Development
+              </p>
+              <a href="#" class="fcg mb-4"
+                >Read More <i class="fa fa-arrow-right"></i
+              ></a>
+            </div>
+          </div>
+          <div class="w-25 ml-15">
+            <div class="p-3">
+              <img src="../assets/UX_UI.png" class="img-size" />
+              <p class="services-content text-white mt-4">
+                UX/UI <br />
+                Design
+              </p>
+              <a href="#" class="fcg mb-4"
+                >Read More <i class="fa fa-arrow-right"></i
+              ></a>
+            </div>
+          </div>
+          <div class="w-25 ml-15">
+            <div class="p-3">
+              <img src="../assets/development.png" class="img-size" />
+              <p class="services-content text-white mt-4">
+                Individual products development
+              </p>
+              <a href="#" class="fcg mb-4"
+                >Read More <i class="fa fa-arrow-right"></i
+              ></a>
+            </div>
+          </div>
+          <div class="w-25 ml-15">
+            <div class="p-3">
+              <img src="../assets/maintain.png" class="img-size" />
+              <p class="services-content text-white mt-4">
+                Long term <br />
+                maintenance
+              </p>
+              <a href="#" class="fcg mb-4"
+                >Read More <i class="fa fa-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-green">
+      <div class="container p-0">
+        <div class="row p-0 m-0">
+          <div class="col-md-6 p-0">
+            <div class="row p-0 m-0">
+              <div class="col-md-12 mt-5 p-0">
+                <p class="m-0 fcgreen font-weight-normal">company</p>
+                <p class="text-white sl ml-0 mt-0 mb-4">Why choose us</p>
+                <div class="col-md-10">
+                  <p class="text-white" style="font-size:18px">
+                    Lorem ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, an unknown printer
+                    took a galley of type.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 p-0">
+            <div class="row m-0 mb-5">
+              <div class="col-md-12 mt-5 p-0">
+                <div class="row m-0">
+                  <div class="col-md-6 bg-blue m-0">
+                    <p class="fc-light-green blockquote box-numeric ml-2 mt-3">
+                      1
+                    </p>
+                    <hr class="bg-white ml-2 mb-0" />
+                    <p class="text-white box-content mr-3 pl-2 mb-5">
+                      We have <span class="fc-light-green">10+</span> years of
+                      experience working with projects of various sizes.
+                    </p>
+                  </div>
+                  <div class="col-md-6 bg-white m-0 ">
+                    <p class="fc-light-green blockquote box-numeric pl-2 mt-3">
+                      2
+                    </p>
+                    <hr class="bg-blue ml-2 mb-0" />
+                    <p class="box-content mr-2 mt-2 mb-5 pl-1 fc-box-content">
+                      We are not only technical guys - we know how to optimize
+                      your business to sell more.
+                    </p>
+                  </div>
+                </div>
+                <div class="row m-0">
+                  <div class="col-md-6 bg-white m-0 ">
+                    <p class="fc-light-green blockquote box-numeric pl-2 mt-3">
+                      3
+                    </p>
+                    <hr class="bg-blue ml-2 mb-0" />
+                    <p class="box-content mr-2 mt-2 pl-1 mb-5 fc-box-content">
+                      We can create a custom technical solutions for your business to make a day easier.
+                    </p>
+                  </div>
+                  <div class="col-md-6 bg-blue m-0">
+                    <p class="fc-light-green blockquote box-numeric ml-2 mt-3">
+                      4
+                    </p>
+                    <hr class="bg-white ml-2 mb-0" />
+                    <p class="text-white box-content mt-2 mr-3 pl-2">
+                      We meet deadlines.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg-white {
+  background-color: white;
+}
+.bg-blue {
+  background-color: #1b2753;
+}
+.fcg {
+  color: #626a89;
+  font-size: 20px;
+  font-weight: 500;
+}
+.fcgreen {
+  color: #08613b;
+}
+.fc-light-green {
+  color: #11c176;
+}
+.sl {
+  font-size: 70px;
+  font-weight: 600;
+}
+.img-size {
+  width: 84px;
+}
+.services-content {
+  margin-top: 15px;
+  font-size: 28px;
+  font-weight: 600;
+}
+.bg-green {
+  background-color: #11c176;
+}
+.box-numeric {
+  font-size: 55px;
+  font-weight: 600;
+}
+.box-content {
+  font-weight: 600;
+  font-size: 21px;
+}
+.box-content span {
+  font-size: 32px;
+  font-weight: 600;
+}
+hr {
+  width: 23%;
+  height: 4px;
+  margin-left: 0;
+  margin-right: auto;
+}
+.fc-box-content {
+  color: #484848;
+}
+</style>
